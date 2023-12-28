e3a2dfee047bb8e1275243422f30986cd7603ab7
StarGrim33 <prosky33@gmail.com>
2023-12-29 02:04:19 +0300

    added layers and two entities

---
+++ FonTech.Domain/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace FonTech.Domain
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ FonTech.Domain/Interfaces/IAuditable.cs
@@ -0,0 +1,13 @@
+﻿namespace FonTech.Domain.Interfaces
+{
+    public interface IAuditable
+    {
+        public DateTime CreatedAt { get; set; }
+
+        public long CreatedBy { get; set; }
+
+        public DateTime LastModifiedAt { get; set; }
+
+        public long UpdatedBy { get; set; }
+    }
+}
