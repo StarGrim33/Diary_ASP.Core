8471a024f2f393359fcccaefcf81c11735e73a61
StarGrim33 <108456723+StarGrim33@users.noreply.github.com>
2024-03-19 20:57:33 +0300

    adding RabbitMQ

--- FonTech.Application/obj/Debug/net8.0/FonTech.Application.AssemblyInfo.cs
+++ FonTech.Application/obj/Debug/net8.0/FonTech.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FonTech.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2ef6fbfc8513433f51cd9a9c695b87e09253d2d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+156bb0ffe510733d939cacbafc774a86e9dcdb2e")]
 [assembly: System.Reflection.AssemblyProductAttribute("FonTech.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FonTech.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
